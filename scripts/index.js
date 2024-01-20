875af4f6587873192fb531c1ff28abe07ae7e9bf
Alejandro Arbelaez <alejandro.devp@outlook.com>
2024-01-19 20:18:42 -0500

    Agregado: Menu de navegacion, movimiento de cajas y selectores globales

---
+++ scripts/index.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // MENU TOGGLE
+    const menu = document.querySelector('#menu')
+    const nav = document.querySelector('#nav')
+
+    menu.addEventListener('click', function() {
+        nav.classList.toggle('visible')
+    })
+
+    // FUNCIONES PARA TARJETA GIRATORIA
+
+    // AGREGAR CLASES A TARJETA
+    const viewMoreButtons = document.querySelectorAll('.viewMore');
+    const faceFrontElements = document.querySelectorAll('.faceFront');
+    const faceBackElements = document.querySelectorAll('.faceBack');
+
+    viewMoreButtons.forEach(function(viewMore, index) {
+        viewMore.addEventListener('click', function() {
+            faceFrontElements[index].classList.add('face', 'front');
+            faceBackElements[index].classList.add('face', 'back');
+        });
+    });
+
+    // QUITAR CLASES A TARJETA
+    const backButtonElements = document.querySelectorAll('.backButton');
+
+    backButtonElements.forEach(function(back, index) {
+        back.addEventListener('click', function() {
+            faceFrontElements[index].classList.remove('face', 'front');
+            faceBackElements[index].classList.remove('face', 'back');
+            faceBackElements[index].classList.add('visible')
+        });
+    });
+
+});
+
+
+
+
+
